9bfffa651d91c06014c9e1bd886f3c5a4c6d5bcd
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-04-03 18:57:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date=require(__dirname + "/date.js");
+
+const app = express();
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+let workitems = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+   const day=date.getDay();
+  res.render("list", {listTitle: day, newListitems: items });
+});
+app.post("/", function (req, res) {
+  const item = req.body.newitem;
+  if (req.body.list === "Work") {
+    workitems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work List", newListitems: workitems });
+});
+app.get("/about",function(req,res){
+    res.render("about");
+});
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
